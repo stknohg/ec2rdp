171dff5de23b4ecbc1d7a05425411dccdd2944bb
stknohg <stknohg@gmail.com>
2025-08-19 16:45:37 +0900

    Update the default environment variables when invoking "aws ec2-instance-connect" command.

--- internal/aws/ec2instanceconnect/ec2instanceconnect.go
+++ internal/aws/ec2instanceconnect/ec2instanceconnect.go
@@ -13,25 +13,23 @@ import (
 func OpenTunnel(cfg aws.Config, ctx context.Context, endpointId string, endpointDnsName string, privateIpAddress string, localPort int, remotePort int) (int, error) {
 	// execute aws ec2-instance-connect open-tunnel command.
 	args := []string{}
-	args = append(args, "ec2-instance-connect")
-	args = append(args, "open-tunnel")
-	args = append(args, "--instance-connect-endpoint-id")
-	args = append(args, endpointId)
-	args = append(args, "--instance-connect-endpoint-dns-name")
-	args = append(args, endpointDnsName)
-	args = append(args, "--private-ip-address")
-	args = append(args, privateIpAddress)
-	args = append(args, "--local-port")
-	args = append(args, strconv.Itoa(localPort))
-	args = append(args, "--remote-port")
-	args = append(args, strconv.Itoa(remotePort))
+	args = append(args,
+		"ec2-instance-connect", "open-tunnel",
+		"--instance-connect-endpoint-id", endpointId,
+		"--instance-connect-endpoint-dns-name", endpointDnsName,
+		"--private-ip-address", privateIpAddress,
+		"--local-port", strconv.Itoa(localPort),
+		"--remote-port", strconv.Itoa(remotePort),
+	)
 	cmd := exec.Command("aws", args...)
 	// pass environment variables in case MFA is required
 	cred, _ := cfg.Credentials.Retrieve(ctx)
-	cmd.Env = append(cmd.Env, fmt.Sprintf("AWS_ACCESS_KEY_ID=%v", cred.AccessKeyID))
-	cmd.Env = append(cmd.Env, fmt.Sprintf("AWS_SECRET_ACCESS_KEY=%v", cred.SecretAccessKey))
-	cmd.Env = append(cmd.Env, fmt.Sprintf("AWS_SESSION_TOKEN=%v", cred.SessionToken))
-	cmd.Env = append(cmd.Env, fmt.Sprintf("AWS_REGION=%v", cfg.Region))
+	cmd.Env = append(os.Environ(),
+		fmt.Sprintf("AWS_ACCESS_KEY_ID=%v", cred.AccessKeyID),
+		fmt.Sprintf("AWS_SECRET_ACCESS_KEY=%v", cred.SecretAccessKey),
+		fmt.Sprintf("AWS_SESSION_TOKEN=%v", cred.SessionToken),
+		fmt.Sprintf("AWS_REGION=%v", cfg.Region),
+	)
 	// start process
 	err := cmd.Start()
 	if err != nil {
